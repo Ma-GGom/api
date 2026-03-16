f7d9e21a946275f9b93d9ac247176279f04dff1b
fakerdeft <whakswp123@naver.com>
2026-03-17 03:08:35 +0900

    fix: 날짜별 로그 파일 및 Gmail SMTP ssl.trust 설정 추가 - logback-spring.xml 추가, prod 프로파일에서 /logs/yyyy-MM-dd.log 날짜별 생성 - GmailFallbackMailConfig에 mail.smtp.ssl.trust 추가

--- app/src/main/kotlin/com/maggom/app/config/GmailFallbackMailConfig.kt
+++ app/src/main/kotlin/com/maggom/app/config/GmailFallbackMailConfig.kt
@@ -25,6 +25,7 @@ class GmailFallbackMailConfig(
                 put("mail.smtp.auth", "true")
                 put("mail.smtp.starttls.enable", "true")
                 put("mail.smtp.starttls.required", "true")
+                put("mail.smtp.ssl.trust", "smtp.gmail.com")
                 put("mail.smtp.connectiontimeout", "5000")
                 put("mail.smtp.timeout", "5000")
                 put("mail.smtp.writetimeout", "5000")
